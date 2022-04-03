a34e49f1a1c3a7d4e92009a55b02af0d085606e8
EasyJBL <kubaprofesor@gmail.com>
2022-04-04 00:06:31 +0200

    Wersja z działającym BFS

--- algorithms/Bfs.py
+++ algorithms/Bfs.py
@@ -2,20 +2,25 @@ class Bfs:
     def __init__(self,structure):
         self.struct = structure
 #
-    def bfs(self,graph):
+    def func(self,graph):
 
         if graph.tovisit or not graph.visited:
             if all(elem in graph.tovisit + graph.visited for elem in graph.graph.neighbors(graph.actual_node)):
                 graph.visited.append(graph.actual_node)
+                graph.size_map[graph.order.index(graph.actual_node)] = 500
                 graph.actual_node = graph.tovisit.pop(0)
+                graph.size_map[graph.order.index(graph.actual_node)] = 1000
             else:
                 for a in graph.graph.neighbors(graph.actual_node):
                     if a not in graph.tovisit and a not in graph.visited:
                         graph.tovisit.append(a)
-                        graph.width[graph.order.index(graph.actual_node), a] =1
-                        print (graph.width)
+                        if (graph.actual_node, a) in graph.width:
+                            graph.width[graph.actual_node, a] =1
+                        else:
+                            graph.width[ a,graph.actual_node] = 1
                         break
         else:
-            graph.visited.append(actual_node)
+            graph.visited.append(graph.actual_node)
+            graph.size_map[graph.order.index(graph.actual_node)] = 500
             graph.actual_node = -1
 
--- graph/Graph.py
+++ graph/Graph.py
@@ -27,7 +27,6 @@ class Graph:
     def loadGraph(self):
         # TODO
         pass
-
     def randomGraph(self, n, density):
         self.n = n
         g=0
@@ -44,13 +43,13 @@ class Graph:
         for node in self.graph:
             self.color_map.append("green")
             self.size_map.append(500)
-        self.position = nx.spring_layout(self.graph,k=4,iterations=1000)
+        self.position = nx.spring_layout(self.graph,k=10,iterations=1000)
         for id in self.position.keys():
             self.order.append(id)
-        self.color_map[self.order.index(self.actual_node)] = "red"
         for u,v in self.graph.edges:
             self.width[u,v] = 0.1
-
+        self.color_map[self.order.index(self.actual_node)] = "red"
+        self.size_map[self.order.index(self.actual_node)] = 1000
     def set_color(self):
         for a in self.tovisit:
             self.color_map[self.order.index(a)] = "yellow"
@@ -64,11 +63,9 @@ class Graph:
                 self.prev_head=self.actual_node
 
     def visualizationGraph(self):
-        #TODO ustawianie grubości krawędzi w zależności czy przechodizmy przez niego stworzyć mapę
-        #TODO tworzenie labelow
         self.data_init()
         nx.draw(self.graph,self.position,node_size=self.size_map,node_color=self.color_map,font_size=11,with_labels=True,
-                labels=self.labels,width=list(self.width.values()))
+                width=list(self.width.values()),labels=self.labels)
         plt.title("Graph n: " + str(self.n))
         alg=Bfs(self)
 
@@ -76,12 +73,12 @@ class Graph:
             if self.actual_node<self.n:
                 self.fig.clear()
                 if not self.actual_node == -1:
-                    alg.bfs(self)
+                    alg.func(self)
                 self.set_color()
-                print(self.color_map)
+
                 nx.draw(self.graph, self.position, node_size=self.size_map, node_color=self.color_map,
-                            font_size=7, with_labels=True, labels=self.labels,width=list(self.width.values()))
-        ani=animation.FuncAnimation(self.fig,animate,interval=3000,repeat=True)
+                            font_size=7, with_labels=True, width=list(self.width.values()),labels=self.labels)
+        ani=animation.FuncAnimation(self.fig,animate, interval=1000,repeat=True)
         plt.show()
     def __str__(self):
         return "=== Graph ===\n" + "n: " + str(self.n)
