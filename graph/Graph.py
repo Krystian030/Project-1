9cb5f32e522d2d5518579fb06966e84f27699700
EasyJBL <kubaprofesor@gmail.com>
2022-04-15 22:55:07 +0200

    Wyświetlanie w tkinter

--- graph/Graph.py
+++ graph/Graph.py
@@ -1,26 +1,30 @@
 import networkx as nx
-from matplotlib import pyplot as plt, animation
 import random
 import time
+import tkinter as Tk
+import copy
+from matplotlib import pyplot as plt, animation
 from algorithms.Bfs import Bfs
 from algorithms.Dfs import Dfs
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
 class Graph:
     def __init__(self):
         self.n = 0
         self.graph = None
         self.fig=plt.figure()
         self.position=None
-        self.color_map = [] #mapa kolorów node'ów
-        self.size_map=[]  #mapa rozmiarów node
-        self.actual_node=0  #pierwszy node
+        self.color_map = []     #mapa kolorów node'ów
+        self.size_map=[]        #mapa rozmiarów node
+        self.actual_node=0      #pierwszy node
         self.width = {}
-        self.degreeMap=[]
         self.labels={}
         self.visited = []   #zmienne obrazujące które wierzchołki musimy jeszcze przejrzeć, a które zostały sprawdzone w całości
         self.tovisit=[]
         self.order=[]  # Rysowanie odbywa się wedle pozycji, a te pozycje zapisane są w tablicy order
-        self.order_label=0  #zmiene pomocnicze w przedzielaniu labeli na nodach
+        self.order_label=0  #zmienne pomocnicze w przedzielaniu labeli na nodach
         self.prev_head=-1
+
     def createRandomGraph(self):
         # TODO
         pass
@@ -31,20 +35,14 @@ class Graph:
 #tworzenie grafu, wyśiwtlanego na ekranie
     def randomGraph(self, n, density):
         self.n = n
-        g=0
         while True:
-            g+=1
-            self.degreeMap.clear()
-            for i in range(0, self.n):
-                self.degreeMap.append(random.randint(1,3))
             self.graph = nx.random_regular_graph(3,self.n)
             if  nx.is_connected(self.graph):
                 break
 #Inicjalizacja daych pomocniczych do grafu
     def data_init(self):
-        for node in self.graph:
-            self.color_map.append("green")
-            self.size_map.append(500)
+        self.color_map = ["green"] * self.n
+        self.size_map = [500]*self.n
         self.position = nx.spring_layout(self.graph,k=10,iterations=1000)
         for id in self.position.keys():
             self.order.append(id)
@@ -67,23 +65,43 @@ class Graph:
 
     def visualizationGraph(self):
         self.data_init()
-
-        nx.draw(self.graph,self.position,node_size=self.size_map,node_color=self.color_map,font_size=11,with_labels=True,
-                width=list(self.width.values()),labels=self.labels)
-        plt.title("Graph n: " + str(self.n))
-
-        alg=Bfs(self)
-        def animate(frame):
-            if self.actual_node<self.n:
-                self.fig.clear()
+        stan_list = []
+        root = Tk.Tk()
+        root.wm_title("Graph n: " + str(self.n))
+        # Quit when the window is done
+        root.wm_protocol('WM_DELETE_WINDOW', root.quit)
+        canvas = FigureCanvasTkAgg(self.fig, master=root)
+        def drawCanvas():
+            self.fig.clear()
+            plt.clf()
+            self.set_color()
+            nx.draw_networkx(self.graph, self.position, node_size=self.size_map, node_color=self.color_map,
+                             font_size=7, with_labels=True, labels=self.labels, width=list(self.width.values()))
+            plt.axis('off')
+            canvas.draw()
+        drawCanvas()
+        canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
+        alg=Dfs(self)
+        def nextGraph():
+            if self.actual_node<self.n and self.actual_node!= -1:
+                stan_list.append(copy.deepcopy(self))
                 if not self.actual_node == -1:
                     alg.func(self)
-                self.set_color()
+            drawCanvas()
 
-                nx.draw(self.graph, self.position, node_size=self.size_map, node_color=self.color_map,
-                            font_size=7, with_labels=True,labels=self.labels, width=list(self.width.values()))
-        ani=animation.FuncAnimation(self.fig,animate, interval=1000,repeat=True)
-        plt.show()
+        def prevGraph():
+            if stan_list:
+                self.__dict__.update(stan_list[-1].__dict__)
+                stan_list.pop()
+                drawCanvas()
+
+        button_next = Tk.Button(root, text="next", command=nextGraph)
+        button_next.pack()
+        button_prev = Tk.Button(root, text="prev", command=prevGraph)
+        button_prev.pack()
+
+        Tk.mainloop()
+
     def __str__(self):
         return "=== Graph ===\n" + "n: " + str(self.n)
 
